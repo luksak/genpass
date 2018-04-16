5693caec0b479c719a134c850cbe1ceb1192f7d1
Lukas von Blarer <lukas.vonblarer@gmail.com>
2018-04-16 12:17:17 +0200

    Initial D8 port

---
+++ src/EventSubscriber/InitSubscriber.php
@@ -0,0 +1,33 @@
+<?php /**
+ * @file
+ * Contains \Drupal\genpass\EventSubscriber\InitSubscriber.
+ */
+
+namespace Drupal\genpass\EventSubscriber;
+
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class InitSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [KernelEvents::REQUEST => ['onEvent', 0]];
+  }
+
+  public function onEvent() {
+    // @FIXME
+// The Assets API has totally changed. CSS, JavaScript, and libraries are now
+// attached directly to render arrays using the #attached property.
+// 
+// 
+// @see https://www.drupal.org/node/2169605
+// @see https://www.drupal.org/node/2408597
+// drupal_add_css(drupal_get_path('module', 'genpass') . '/genpass.css');
+    $build['#attached']['library'][] = 'genpass/genpass.libraries.yml';
+
+  }
+
+}
---
+++ src/InitSubscriber.php
@@ -0,0 +1,26 @@
+<?php
+namespace Drupal\genpass;
+
+class InitSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [KernelEvents::REQUEST => ['onEvent', 0]];
+  }
+
+  public function onEvent() {
+    // @FIXME
+// The Assets API has totally changed. CSS, JavaScript, and libraries are now
+// attached directly to render arrays using the #attached property.
+// 
+// 
+// @see https://www.drupal.org/node/2169605
+// @see https://www.drupal.org/node/2408597
+// drupal_add_css(drupal_get_path('module', 'genpass') . '/genpass.css');
+    $build['#attached']['library'][] = 'genpass/genpass.libraries.yml';
+
+  }
+
+}
